05b008d1a9c301a689645398e4ff73a2b3d7f61b
Fernando Papito <fernandorcks@gmail.com>
2023-12-18 18:15:31 -0300

    campos obrigatórios

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -14,3 +14,21 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     await page.movies.create(movie)
     await page.toast.containText('Cadastro realizado com sucesso!')
 })
+
+test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({ page }) => {
+
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
+
+    await page.movies.goForm()
+    await page.movies.submit()
+
+    await page.movies.alertHaveText([
+        'Por favor, informe o título.',
+        'Por favor, informe a sinopse.',
+        'Por favor, informe a empresa distribuidora.',
+        'Por favor, informe o ano de lançamento.'
+    ])
+
+})
