58ef085ac28fa95877689dd500ec840d28c4a45d
Fernando Papito <fernandorcks@gmail.com>
2023-12-21 16:13:57 -0300

    cenário de filme duplicado

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -3,13 +3,27 @@ const { test } = require('../support')
 const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
-test('deve poder cadastrar um novo filme', async ({ page, request }) => {
+test.beforeAll(async () => {
+    await executeSQL(`DELETE from movies`)
+})
+
+test('deve poder cadastrar um novo filme', async ({ page }) => {
     const movie = data.create
-    await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`)
 
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.create(movie)
     await page.toast.containText('Cadastro realizado com sucesso!')
+})
+
+test('não deve cadastrar quando o título é duplicado', async ({ page }) => {
+    const movie = data.duplicate
+
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.create(movie)
+    await page.toast.containText('Cadastro realizado com sucesso!')
+
+    await page.movies.create(movie)
+    await page.toast.containText('Este conteúdo já encontra-se cadastrado no catálogo')
 })
 
 test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({ page }) => {
