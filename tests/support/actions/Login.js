ad95db9faa2fc784b1ba63daefebb49577f5f6f8
Fernando Papito <fernandorcks@gmail.com>
2023-12-28 12:57:49 -0300

    Resolvendo o bug na função "do"

--- tests/support/actions/Login.js
+++ tests/support/actions/Login.js
@@ -7,9 +7,9 @@ export class Login {
     }
 
     async do(email, password, username) {
-        this.visit()
-        this.submit(email, password)
-        this.isLoggedIn(username)
+        await this.visit()
+        await this.submit(email, password)
+        await this.isLoggedIn(username)
     }
 
     async visit() {
